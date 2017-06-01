23d9a2821e380139ee579bf6fae3b706c8ec1333
Hack Reactor Student <student@hackreactor.com>
2017-05-31 21:41:27 -0700

    VideoListEntry and VideoListView tests passed

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,7 +1,19 @@
 var VideoListView = Backbone.View.extend({
 
+//start a initialize function
+  //we want to render every data (which should be 5)
+    //we get frin
+  initialize: function() {
+
+    this.collection.on('sync', this.render, this);  
+  
+  },
 
   render: function() {
+    _.forEach(this.collection.models, function(entry) {
+      new VideoListEntryView(entry).render();
+    });
+
     this.$el.children().detach();
     this.$el.html(this.template());
     return this;
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,9 +1,20 @@
 var VideoListEntryView = Backbone.View.extend({
 
+  events: {
+    'click .video-list-entry-title': 'handleClick'
+  },
 
   render: function() {
-    this.$el.html(this.template());
+    //console.log(this);
+    // console.log(this.template(this.model.toJSON()));
+    //console.log(this.template(this.model));
+    this.$el.html(this.template({snippet: this.model}));
     return this;
+  },
+
+  handleClick: function() {
+    console.log('hi');
+    this.model.select();
   },
 
   template: templateURL('src/templates/videoListEntry.html')
